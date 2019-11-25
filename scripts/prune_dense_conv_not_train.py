8debdff0e4620e6228b2db356e2cb70ee40cfbcc
kaisei <kaise64taka84@keio.jp>
2019-11-25 13:43:54 +0900

    not_trainを追加

--- scripts/prune_dense_conv_not_train.py
+++ scripts/prune_dense_conv_not_train.py
@@ -54,7 +54,7 @@ for count in range(1, inv_prune_ratio):
     for i in range(conv_count):
         print(f'channel_number{i + 1}: {channel_num_new[i]}', end=", " if i != conv_count - 1 else "\n")
 
-    f_num_epochs = 5
+    f_num_epochs = 1
     # finetune
     start = time.time()
     for epoch in range(f_num_epochs):
