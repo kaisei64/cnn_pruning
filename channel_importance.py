2f2b4c0ee95ead3acd997952c35ce1e5496a7524
kaisei <kaise64taka84@keio.jp>
2019-11-29 14:56:31 +0900

    リファクタリング

--- channel_importance.py
+++ channel_importance.py
@@ -3,13 +3,14 @@ import torch.nn as nn
 import numpy as np
 from result_save_visualization import *
 
+# パラメータ利用
+net = parameter_use('./result/CIFAR10_original_train.pkl')
+# 畳み込み層のリスト
+conv_list = [net.features[i] for i in range(len(net.features)) if
+             isinstance(net.features[i], nn.Conv2d)]
+
 
 def channel_importance(conv_num):
-    # パラメータ利用
-    net = parameter_use('./result/CIFAR10_original_train.pkl')
-    # 畳み込み層のリスト
-    conv_list = [net.features[i] for i in range(len(net.features)) if
-                 isinstance(net.features[i], nn.Conv2d)]
     # 勾配と重みの積を保持
     grad_weight_multi_for_each_layer = [conv_list[i].weight.data.cpu().numpy() * conv_list[i].grad.data.cpu().numpy()
                                         for i in range(len(conv_list))]
