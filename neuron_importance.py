35d96c700bb09b2d1263df1c0d5b6e2020cb1bc2
kaisei <kaise64taka84@keio.jp>
2019-12-11 11:07:39 +0900

    リファクタリング

--- neuron_importance.py
+++ neuron_importance.py
@@ -17,14 +17,14 @@ for i, dense in enumerate(dense_list):
 
 def neuron_importance(dense_num):
     # ニューロン重要度が上位20%と下位5%を保持
-    de_high_20 = [list() for _ in range(len(dense_list))]
-    de_low_5 = [list() for _ in range(len(dense_list))]
+    de_high = [list() for _ in range(len(dense_list))]
+    de_low = [list() for _ in range(len(dense_list))]
 
     with torch.no_grad():
         for i in range(len(dense_list)):
-            de_high_20[i] = np.argsort(l1norm_list[i])[:int(dense_list[i].out_features / 4)]
-            de_low_5[i] = np.argsort(l1norm_list[i])[int(dense_list[i].out_features * 19 / 20):]
-    return de_high_20[dense_num], de_low_5[dense_num]
+            de_high[i] = np.argsort(l1norm_list[i])[:int(dense_list[i].out_features / 4)]
+            de_low[i] = np.argsort(l1norm_list[i])[int(dense_list[i].out_features * 19 / 20):]
+    return de_high[dense_num], de_low[dense_num]
 
 
 def neuron_euclidean_distance(ne1, ne2):
