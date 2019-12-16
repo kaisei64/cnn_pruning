ad90114b9b70982451428300541275a0cc69f582
kaisei <kaise64taka84@keio.jp>
2019-12-16 14:12:10 +0900

    モデルを修正

--- model.py
+++ model.py
@@ -1,87 +1,87 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
-num_classes = 10
-
-device = 'cuda'
-dtype = torch.float
+# import torch.nn.functional as F
 
 
-class MyConv2d(nn.Conv2d):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True,
-                 padding_mode='zeros'):
-        super().__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias, padding_mode)
-        self.mask = None
-
-    def forward(self, input, flag, mask):
-        self.mask = mask
-        if not flag:
-            if self.padding_mode == 'circular':
-                expanded_padding = ((self.padding[1] + 1) // 2, self.padding[1] // 2,
-                                    (self.padding[0] + 1) // 2, self.padding[0] // 2)
-                return F.conv2d(F.pad(input, expanded_padding, mode='circular'),
-                                self.weight, self.bias, self.stride,
-                                _pair(0), self.dilation, self.groups)
-            return F.conv2d(input, self.weight, self.bias, self.stride,
-                            self.padding, self.dilation, self.groups)
-        else:
-            mask = torch.tensor(self.mask, device=device, dtype=dtype)
-            with torch.no_grad():
-                self.weight *= mask
-            if self.padding_mode == 'circular':
-                expanded_padding = ((self.padding[1] + 1) // 2, self.padding[1] // 2,
-                                    (self.padding[0] + 1) // 2, self.padding[0] // 2)
-                return F.conv2d(F.pad(input, expanded_padding, mode='circular'),
-                                self.weight, self.bias, self.stride,
-                                _pair(0), self.dilation, self.groups)
-            return F.conv2d(input, self.weight, self.bias, self.stride,
-                            self.padding, self.dilation, self.groups)
-
-
-class Mylinear(nn.Linear):
-    def __init__(self, in_features, out_features, bias=True):
-        super().__init__(in_features, out_features, bias)
-        self.mask = None
-
-    def forward(self, input, flag, mask):
-        self.mask = mask
-        if not flag:
-            return F.linear(input, self.weight, self.bias)
-        else:
-            # print(mask.shape)
-            # print(self.weight.shape)
-            # print(self.bias.shape)
-            # print(input.shape)
-            mask = torch.t(torch.tensor(self.mask, device=device, dtype=dtype))
-            return F.linear(input, self.weight * mask, self.bias)
+# class Conv2d(nn.Conv2d):
+#     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True,
+#                  padding_mode='zeros'):
+#         super().__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias, padding_mode)
+#         self.mask = None
+#
+#     def forward(self, input, flag, mask):
+#         self.mask = mask
+#         if not flag:
+#             if self.padding_mode == 'circular':
+#                 expanded_padding = ((self.padding[1] + 1) // 2, self.padding[1] // 2,
+#                                     (self.padding[0] + 1) // 2, self.padding[0] // 2)
+#                 return F.conv2d(F.pad(input, expanded_padding, mode='circular'),
+#                                 self.weight, self.bias, self.stride,
+#                                 _pair(0), self.dilation, self.groups)
+#             return F.conv2d(input, self.weight, self.bias, self.stride,
+#                             self.padding, self.dilation, self.groups)
+#         else:
+#             mask = torch.tensor(self.mask, device=device, dtype=dtype)
+#             with torch.no_grad():
+#                 self.weight *= mask
+#             if self.padding_mode == 'circular':
+#                 expanded_padding = ((self.padding[1] + 1) // 2, self.padding[1] // 2,
+#                                     (self.padding[0] + 1) // 2, self.padding[0] // 2)
+#                 return F.conv2d(F.pad(input, expanded_padding, mode='circular'),
+#                                 self.weight, self.bias, self.stride,
+#                                 _pair(0), self.dilation, self.groups)
+#             return F.conv2d(input, self.weight, self.bias, self.stride,
+#                             self.padding, self.dilation, self.groups)
+#
+#
+# class Mylinear(nn.Linear):
+#     def __init__(self, in_features, out_features, bias=True):
+#         super().__init__(in_features, out_features, bias)
+#         self.mask = None
+#
+#     def forward(self, input, flag, mask):
+#         self.mask = mask
+#         if not flag:
+#             return F.linear(input, self.weight, self.bias)
+#         else:
+#             # print(mask.shape)
+#             # print(self.weight.shape)
+#             # print(self.bias.shape)
+#             # print(input.shape)
+#             mask = torch.t(torch.tensor(self.mask, device=device, dtype=dtype))
+#             return F.linear(input, self.weight * mask, self.bias)
 
 
 class AlexNet(nn.Module):
     def __init__(self, num_classes):
         super(AlexNet, self).__init__()
-        self.conv1 = MyConv2d(3, 64, kernel_size=11, stride=4, padding=5)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        # self.batchnorm1 = nn.BatchNorm2d(64)
-        # self.batchnorm2 = nn.BatchNorm2d(192)
-        # self.batchnorm5 = nn.BatchNorm2d(256)
-        self.conv2 = MyConv2d(64, 192, kernel_size=5, padding=2)
-        self.conv3 = MyConv2d(192, 384, kernel_size=3, padding=1)
-        self.conv4 = MyConv2d(384, 256, kernel_size=3, padding=1)
-        self.conv5 = MyConv2d(256, 256, kernel_size=3, padding=1)
-        self.fc1 = Mylinear(256, 100)
-        self.fc2 = Mylinear(100, num_classes)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=5)
+        self.conv2 = nn.Conv2d(64, 192, kernel_size=5, padding=2)
+        self.conv3 = nn.Conv2d(192, 384, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(384, 256, kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.conv1_coef = torch.ones(64)
+        self.conv2_coef = torch.ones(192)
+        self.conv3_coef = torch.ones(384)
+        self.conv4_coef = torch.ones(256)
+        self.conv5_coef = torch.ones(256)
+        self.pool = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.fc1 = nn.Linear(256 * 6 * 6, 4096)
+        self.fc1 = nn.Linear(4096, 4096)
+        self.fc2 = nn.Linear(4096, num_classes)
+        self.drop = nn.Dropout()
+        self.activation = nn.ReLU(inplace=True)
 
-    def forward(self, x, flag_c, flag_f, mask1, mask2, mask3, mask4, mask5, mask6, mask7):
-        x = self.pool(F.relu(self.conv1(x, flag_c, mask1)))
-        # x = self.batchnorm1(self.pool(F.relu(self.conv1(x, flag_c, mask1))))
-        x = self.pool(F.relu(self.conv2(x, flag_c, mask2)))
-        # x = self.batchnorm2(self.pool(F.relu(self.conv2(x, flag_c, mask2))))
-        x = self.pool(F.relu(self.conv3(x, flag_c, mask3)))
-        x = self.pool(F.relu(self.conv4(x, flag_c, mask4)))
-        x = self.pool(F.relu(self.conv5(x, flag_c, mask5)))
-        # x = self.batchnorm5(self.pool(F.relu(self.conv5(x, flag_c, mask5))))
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x, flag_f, mask6))
-        x = self.fc2(x, flag_f, mask7)
+    def forward(self, x, flag, coef):
+        x = self.pool(self.activation(self.conv1(x) * self.conv1_coef))
+        x = self.pool(self.activation(self.conv2(x) * self.conv2_coef))
+        x = self.activation(self.conv3(x) * self.conv3_coef)
+        x = self.activation(self.conv4(x) * self.conv4_coef)
+        x = self.pool(self.activation(self.conv5(x) * self.conv5_coef))
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.activation(self.fc1(self.drop(x)))
+        x = self.activation(self.fc2(self.drop(x)))
+        x = self.fc3(x)
         return x
