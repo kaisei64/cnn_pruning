ae4ad5fb08f92c631fd5be47ed251f9c29869206
kaisei <kaise64taka84@keio.jp>
2019-12-20 11:06:57 +0900

    変数名を変更＋重要度が低いチャネルも選択するよう変更

--- pfgacnn_mymodel.py
+++ pfgacnn_mymodel.py
@@ -30,10 +30,10 @@ class PfgaCnn:
         # a_min = np.min(a)
         # y = 2 * (a - a_min) / (a_max - a_min) - 1
         # チャネル重要度が上位10%の個体を初期個体にする
-        ch_high10, ch_low5 = channel_importance(self.conv_num)
+        ch_high, ch_low = channel_importance(self.conv_num)
         # 選択されるチャネルのindex
-        # ch_index = random.choice(np.concatenate([ch_high10, ch_low5]))
-        ch_index = random.choice(ch_high10)
+        ch_index = random.choice(np.concatenate([ch_high, ch_low]))
+        # ch_index = random.choice(ch_high10)
         new_gene.append(conv_list[self.conv_num].weight.data.clone().cpu().detach().numpy()[ch_index, :, :, :])
         new_gene.append(None)
         self.family.append(new_gene)
