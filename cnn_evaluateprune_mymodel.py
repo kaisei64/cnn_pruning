e1d9c698c82955e9d2f43f8d3c6d2f16f8f5a0f6
kaisei <kaise64taka84@keio.jp>
2019-12-27 15:20:41 +0900

    保存ファイル名を変更

--- cnn_evaluateprune_mymodel.py
+++ cnn_evaluateprune_mymodel.py
@@ -121,7 +121,7 @@ class CnnEvaluatePrune:
 
             # 結果の保存
             input_data = [g_count, epoch + 1, avg_train_loss, avg_train_acc, avg_val_loss, avg_val_acc]
-            result_save('./result/add_channels_not_train_mymodel.csv', data_dict, input_data)
+            result_save('./result/add_channels_train_mymodel.csv', data_dict, input_data)
 
         return 10000 * eva + similarity
         # return eva
